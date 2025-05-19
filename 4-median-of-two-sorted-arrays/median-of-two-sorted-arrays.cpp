6d066f0f399b90e7ea4a6d6319c52da5ea70b4a5
Saiteja Lukka <154242674+Nimbusprogrammer@users.noreply.github.com>
2025-05-19 23:11:12 +0530

    Time: 0 ms (100.00%) | Memory: 94.9 MB (93.54%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,59 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // BRUTE FORCE : 2-POINTER TECHNIQUE.
-        int n = nums1.size();
-        int m = nums2.size();
-        int mid1;
-        int mid2;
-        int midindex1 = (m + n) / 2;
-        int midindex2 = (m + n) / 2 - 1;
-
-        int left = 0;
-        int realindex = 0;
-        int right = 0;
-        double answer;
-        while (left <= n - 1 && right <= m - 1) {
-            if (nums1[left] <= nums2[right]) {
-                if (realindex == midindex1) {
-                    mid1 = nums1[left];
+            int n1 = nums1.size();
+            int n2 = nums2.size();
+            if ( n1> n2) return findMedianSortedArrays( nums2 , nums1);
+            int left = ( n1+n2)/2;//these are the number of numbers that are on the 
+            //left side of merged array
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+            //binary search on the number of digits that can be taken from the samllest array to form the left side of merged array.
+            int low = 0;//nothing 
+            int high = n1;
+            while ( low <= high)
+            {
+                int mid1 = (low+high)/2;//no of numbers from nums1 that form left side of merged array.
+                int mid2 = left - mid1;
+                if ( mid1-1 >= 0)  l1= nums1[mid1-1];
+                else l1 = INT_MIN;
+                if ( mid2-1  >= 0)  l2 =  nums2[mid2-1];
+                else l2 = INT_MIN;
+                if (  mid1 < n1) r1 = nums1[mid1];
+                else r1 = INT_MAX;
+                if ( mid2 < n2) r2 = nums2[mid2];
+                else r2 = INT_MAX;
+                if (l1 <= r2 && l2 <= r1)
+                {
+                    if ( (n1+n2)%2 == 0)
+                    return ( max(l1,l2) + min(r1,r2))/2.0;
+                    else 
+                    return min(r1,r2);
                 }
-                if (realindex == midindex2)
-                    mid2 = nums1[left];
-                left++;
-                realindex++;
-            } else {
-                if (realindex == midindex1) {
-                    mid1 = nums2[right];
-                }
-                if (realindex == midindex2)
-                    mid2 = nums2[right];
-                right++;
-                realindex++;
+                else if ( l1 > r2 )
+                high = mid1-1;
+                else 
+                low = mid1+1;
             }
-        }
-        while (left <= n - 1) {
-            if (realindex == midindex1) {
-                mid1 = nums1[left];
-            }
-            if (realindex == midindex2)
-                mid2 = nums1[left];
-            realindex++;
-            left++;
-        }
-        while (right <= m - 1) {
-            if (realindex == midindex1) {
-                mid1 = nums2[right];
-            }
-            if (realindex == midindex2)
-                mid2 = nums2[right];
-            right++;
-            realindex++;
-        }
-        if ((m + n) % 2 == 0) {
-            answer =(double) (mid1 + mid2) / 2;
-        } else
-            answer = mid1;
-        return answer;
+            return 0;
+                   
     }
 };

0ff5564dd373c0c7a0b36de05fecf357f7a2ac81
Saiteja Lukka <154242674+Nimbusprogrammer@users.noreply.github.com>
2025-07-21 18:26:13 +0530

    Time: 2 ms (61.43%) | Memory: 13.9 MB (99.27%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -4,31 +4,30 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans ;
         int i = 0;
+        int sum = 0;
         vector<int> temp;
-         combosum( candidates , target , i , ans , temp);
+         combosum( candidates , target , i , ans , temp , sum);
          return ans;
     }
-    void combosum(vector<int>& candidates  , int target ,  int i , vector<vector<int>> &ans , vector<int> temp)
+    void combosum(vector<int>& candidates  , int target ,  int i , vector<vector<int>> &ans , vector<int>& temp , int &sum)
     {
-        int sum = 0;
-            for( auto it : temp )
-            {
-                 sum += it;
-            }  
-            if( sum == target)
-            {
-                ans.push_back(temp);
-                return;
-            }
-        if ( i > candidates.size()-1 || sum > target)
+        if ( sum == target)
+         {
+            ans.push_back(temp);
+         } 
+         
+        if ( i > candidates.size()-1 || sum >= target)
         { 
+           
            return;
         }
-         
+           
         temp.push_back(candidates[i]);
-        combosum( candidates , target , i , ans , temp );
+        sum+=candidates[i];
+        combosum( candidates , target , i , ans , temp ,sum );
         temp.pop_back();
-        combosum(candidates , target , i+1 , ans , temp);
+        sum-=candidates[i];
+        combosum(candidates , target , i+1 , ans , temp, sum);
         
     }
     

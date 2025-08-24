e903353c5e780c0d5a44ca7a78dded32e9d71d66
Saiteja Lukka <154242674+Nimbusprogrammer@users.noreply.github.com>
2025-08-24 16:23:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (36.07%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-int factorial ( int j)
-{
-    
-    int fact = 1;
-    for  (int i = 1; i <= j ; i++)
-    {
-        fact *= i;
-    }
-   return fact;
-}
+
     string getPermutation(int n, int k) {
         string ans;
         vector<int> nums(n+1);
+        vector<int> factorial(n+1);
+        factorial[0] =1 ;
         for ( int i = 0 ;i < n ;i++)
         {
             nums[i] = i+1;
+            if ( i > 0)
+            factorial[i] = factorial[i-1]*i;
+            
         }
         int r = k-1;
         for ( int i = n ; i > 0 ; i--)
         {
-             int blocksize =  factorial(i-1);
+             int blocksize =  factorial[i-1];
              int blockindex = r/blocksize;
              ans += to_string(nums[blockindex]);
              nums.erase( nums.begin() + blockindex);

5c8eb08a4606ff031b0075ea1fe0906f3efa2f52
Saiteja Lukka <154242674+Nimbusprogrammer@users.noreply.github.com>
2025-08-30 22:01:32 +0530

    Time: 3 ms (42.03%) | Memory: 12.2 MB (91.50%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-            vector<int> temp;
-            int n = nums1.size();
-            vector<int> ans;
-            vector<int> output;
-            stack<int> st;
-            int m = nums2.size();
-            for ( int i = 0 ; i < n ; i++)
+        // brute force method
+        vector<int> ans;
+        int n = nums1.size();
+        int m = nums2.size();
+        // bool present = false; this has to be updated everytime inside for loop and should start with false until updated
+        int index; // to keep track of the index of its position int nums 2 of
+                   // element of nums1
+        for (int i = 0; i < n; i++)
+         {
+            bool present = false;
+            for (int j = 0; j < m; j++) {
+                if (nums1[i] == nums2[j]) {
+                    index = j;
+                    present = true;
+                    break;
+                }
+            }
+            if (present) // search for next greatest element
             {
-                for ( int j = 0 ; j < m ; j++)
+                for (int k = index ; k < m; k++) //start k with index as if index is direclty last element like in nums1 2,1,3 if and 3 is at last of nums2 then we should not skip it and fill -1 as no other number after 3 is greater than 3 to its right side .
                 {
-                    if ( nums1[i] == nums2[j])
+                    if (nums2[k] > nums2[index])
                     {
-                          temp.push_back(j);
-                          break;
+                        ans.push_back(nums2[k]);
+                        break;
+                    }
+                    else if (k == m-1) {
+                        ans.push_back(-1);
                     }
                 }
             }
-            for ( int j = m-1 ;j >= 0 ; j--)
-            {
-                
-                   
-                
-                while( !st.empty() && st.top() <= nums2[j])
-                {
-                    st.pop();
-                }
-                //result for the next big num should be checked after removing smaller elements from stack
-                 if ( !st.empty() )
-                    {
-                        if ( st.top() > nums2[j])
-                    output.push_back(st.top());
-                    else 
-                    output.push_back(-1);
-                    }
-                    else 
-                    output.push_back(-1);
-
-                if ( st.empty() || nums2[j] < st.top())
-                {
-                    st.push(nums2[j]);
-                }
-            }
-            reverse(output.begin() , output.end());
-            for ( int i = 0 ; i < n ; i++)
-            {
-               ans.push_back(output[temp[i]]);
-            }
-            return ans;
+        }
+        return ans;
     }
 };

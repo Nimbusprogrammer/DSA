ac56c100dd57c7d98c3c005c6dd78549bf824f9e
Saiteja Lukka <154242674+Nimbusprogrammer@users.noreply.github.com>
2025-08-31 23:09:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (61.00%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        // brute force method
-        vector<int> ans;
-        int n = nums1.size();
-        int m = nums2.size();
-        // bool present = false; this has to be updated everytime inside for loop and should start with false until updated
-        int index; // to keep track of the index of its position int nums 2 of
-                   // element of nums1
-        for (int i = 0; i < n; i++)
+         int n = nums1.size();
+         int m = nums2.size();
+         vector<int> ans;
+         //to maintain hashmap key(element from nums2), value(its corresponding ngm)
+         unordered_map<int , int > nextgreater;
+         stack<int> st;
+         for ( int i = m-1 ; i >= 0 ; i--)
          {
-            bool present = false;
-            for (int j = 0; j < m; j++) {
-                if (nums1[i] == nums2[j]) {
-                    index = j;
-                    present = true;
-                    break;
-                }
-            }
-            if (present) // search for next greatest element
-            {
-                for (int k = index ; k < m; k++) //start k with index as if index is direclty last element like in nums1 2,1,3 if and 3 is at last of nums2 then we should not skip it and fill -1 as no other number after 3 is greater than 3 to its right side .
-                {
-                    if (nums2[k] > nums2[index])
-                    {
-                        ans.push_back(nums2[k]);
-                        break;
-                    }
-                    else if (k == m-1) {
-                        ans.push_back(-1);
-                    }
-                }
-            }
-        }
-        return ans;
+              while( !st.empty() && st.top() <= nums2[i])
+              {
+                st.pop();
+              }
+              if ( st.empty() || st.top() > nums2[i])
+              {
+                if ( !st.empty())
+                  nextgreater[nums2[i]] = st.top();
+                  else 
+                  nextgreater[nums2[i]] = -1;
+                  
+                  
+              }
+              st.push(nums2[i]);
+         }
+         for ( int i = 0 ; i < n ;i++)
+         {
+            ans.push_back(nextgreater[nums1[i]]);
+         }
+         return ans;
     }
 };

df07b40009b0e1ccf49550f8d9ee7f2af922310e
Saiteja Lukka <154242674+Nimbusprogrammer@users.noreply.github.com>
2025-09-19 10:44:49 +0530

    Time: 31 ms (59.19%) | Memory: 92 MB (89.26%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,15 +1,17 @@
 class StockSpanner {
-    stack<pair<int,int>> st; // (price, span)
 public:
-    StockSpanner() { }
-    
-    int next(int price) {
+    StockSpanner() {
+        
+    }
+    stack<pair<int,int>> st;//don't think to loop arr[st.top()] like that instead you are storing all inside the stack including element and its span.As stack only has ascending order the span of top most element is will be sum of spans of all lower elements until its bigger that top as eg : 5 , 4 ,3, ,2 here when we do 8 as next element then span of 2 will cover >= 2 then span of 3 will cover that so on so no double counting
+    int next(int price) { 
         int span = 1;
-        while (!st.empty() && st.top().first <= price) {
-            span += st.top().second;
+        while ( !st.empty() && st.top().first <= price ){
+            span+= st.top().second;
             st.pop();
+            
         }
-        st.push({price, span});
+        st.push({price , span});
         return span;
     }
 };

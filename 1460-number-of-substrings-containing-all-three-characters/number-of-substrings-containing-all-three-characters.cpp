a53ea802b62cd604f9b1dd515185923d7c65dcf5
Saiteja Lukka <154242674+Nimbusprogrammer@users.noreply.github.com>
2025-10-20 16:07:46 +0530

    Time: 15 ms (35.88%) | Memory: 11.1 MB (58.59%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -5,7 +5,8 @@ public:
         int count = 0;
           for ( int i = 0 ; i < s.size() ; i++){
                  hash[s[i]-'a'] = i;
-                 count += 1+ min( hash[0 ] , min( hash[1] , hash[2]));
+                 //count += 1+ min( hash[0 ] , min( hash[1] , hash[2]));
+                 count += 1+ min({hash[0]  , hash[1] , hash[2]});
           }
           return count;
     }
